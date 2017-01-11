3af858a8e319c654c8e89f64535bdac788dda644
rogeruwayezu <rogeruwayezu@gmail.com>
2017-01-11 18:15:51 +0200

    completing the exercise

--- example.rb
+++ example.rb
@@ -5,22 +5,32 @@ class Car
     @price = price
     @color = color
   end
+
   def make=(text)
     @make = text
   end
   def make
     @make
   end
+
   def model
     @model
+  end
+
+  def price=(number)
+    @price = number
   end
   def price
     @price
   end
+
   def color
     @color
   end
 
 end
-car1 = Car.new("Toyota", "color", 2000, "black")
-puts "the model of this car is #{}"
+car1 = Car.new("Toyota", "Colora", 2000, "black")
+puts car1.model
+puts car1.color
+puts car1.make=("BMW")
+puts car1.price=(5000)
